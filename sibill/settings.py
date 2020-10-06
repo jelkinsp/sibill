2787a4a75c8eb523b1de7d62cf1a18f95b85dd25
Jose Luis <jlluengoramos@gmail.com>
2020-10-06 21:48:02 +0200

    0.0.0 - Ultimos ajustes

--- sibill/settings.py
+++ sibill/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'sibill.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST':'127.0.0.1',
+        'DATA_BASE':'5432',
+
     }
 }
 
