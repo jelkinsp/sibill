f3ea7fb517dbd8beb732bb9160e3ba6826265408
Jose Luis <jlluengoramos@gmail.com>
2020-10-08 12:06:24 +0200

    1.2.0 - Documentacion añadida.

--- sibill/settings.py
+++ sibill/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'sibill_api',
+    'rest_framework_swagger',
 ]
 
 MIDDLEWARE = [
--- sibill_api/urls.py
+++ sibill_api/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
+from rest_framework.schemas import get_schema_view
+
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-
-    path('invoice/', views.manage_invoice ),
+    path('invoice/', views.manage_invoice),
     path('invoice/<int:invoice_id>/', views.get_invoice_id),
     path('invoice/<str:user_name>/<int:year_invoice>/', views.get_invoice_average),
+    path('openapi', get_schema_view(
+        title="Sibill",
+        description="Prueba tecnica",
+        version="1.2.0"
+    ), name='openapi-schema'),
 ]
+
